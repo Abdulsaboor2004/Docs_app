bf3ea96b5ef3f0fcf09bc6b5f058ae827f7a77dc
Abdul Saboor <abdulsaboor.17.4.2004@gmail.com>
2024-03-20 04:47:43 +0500

    added GA4 using React-ga

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import React from "react";
-import Background from "./components/Background";
-import Foreground from "./components/Foreground";
+import { React, useEffect } from 'react';
+import ReactGA from 'react-ga';
+import Background from './components/Background';
+import Foreground from './components/Foreground';
 
+ReactGA.initialize('G-BJMFZWTTXK');
 
 const App = () => {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
-    
-    <div className="relative w-full h-screen bg-zinc-800">
+    <div className='relative w-full h-screen bg-zinc-800'>
       <Background />
       <Foreground />
     </div>
-    
   );
 };
 
